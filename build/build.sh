9a4524b2d79dddd30ee127de7b3e609a3385a56e
Jae Bradley <jae.b.bradley@gmail.com>
2022-06-06 00:42:42 -0400

    Use non relative path

--- build/build.sh
+++ build/build.sh
@@ -7,8 +7,7 @@ main() {
   . "$(dirname "${BASH_SOURCE[0]}")/../install.sh" "$(dirname "${BASH_SOURCE[0]}")/../dependencies"
   if [[ ! $? ]]; then fail "Installation failed"; fi
 
-  $(lint "$(dirname "${BASH_SOURCE[0]}")/../dependencies/.executables/shellcheck-v0.8.0/shellcheck" "$(dirname "${BASH_SOURCE[0]}")/..")
-  if [[ ! $? ]]; then fail "Linting failed"; fi
+  lint "$(dirname "${BASH_SOURCE[0]}")/../dependencies/.executables/shellcheck-v0.8.0/shellcheck" "$(dirname "${BASH_SOURCE[0]}")/.." || fail "Linting failed"
 }
 
 main
