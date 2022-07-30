1b7d4f220969ee36824129c88d751430b30c81c7
Jae Bradley <jae.b.bradley@gmail.com>
2022-07-30 09:44:41 -0400

    Add test

--- utilities/fail.sh
+++ utilities/fail.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 function fail() {
-  if [[ "$1" != "" ]]; then printf "$1\n"; fi
+  if [[ $# -gt 1 ]]; then printf "Expected exactly zero arguments or one argument, a message\n" && exit 255; fi
+  if [[ "1" == "$#" ]]; then printf "$1\n"; fi
   exit 255
 }
